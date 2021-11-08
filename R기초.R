10fd253735e8770feac61836fec8d794236a61a5
Jong-SeongLee <52143255+Jong-SeongLee@users.noreply.github.com>
2021-11-08 19:21:12 +0900

    Update R기초.R

--- R기초.R
+++ R기초.R
@@ -1,4 +1,4 @@
-# 3.2 벡터와 연산자 
+#2021.3.17
 
 # 벡터 만들기
 score <- 70
@@ -20,7 +20,7 @@ name <- c("알라딘", "자스민", "지니")
 name
 str(name)
 
-# 연속적인 값들의 벡터 만들기
+# 연속적인 값 벡터 만들기
 x <- seq(1, 10, by=3)
 x
 
@@ -46,7 +46,7 @@ x
 x <- seq(1, 10, by=2.25) 
 x
 
-# 반복적인 값들의 벡터 만들기
+# 반복적인 값 벡터 만들기
 x <- c(1, 2, 3)
 x
 y <- rep(x, times=2)
@@ -54,7 +54,7 @@ y
 y <- rep(x, each=2)
 y
 
-# 산술 연산
+# 연산
 x <- 2^3
 x
 x <- 2 * 2 * 2
@@ -64,7 +64,7 @@ x
 x <- 2-3
 x
 
-# 벡터와 사칙 연산
+# 벡터, 사칙 연산
 x <- c(10, 20, 30, 40)
 x
 y <- c(2, 4, 6, 8)
@@ -82,7 +82,7 @@ x+y
 x
 x+z
 
-# 비교 연산자 
+# 비교 연산 
 3 < 10
 
 5 > 10
@@ -94,13 +94,13 @@ x <= 10
 x[x>15]
 x <- x[x>15]
 x
-# 비교 연산자 
+# 비교 연산
 x <- c(10, 20, 30)
 any(x <= 10)
 
 all(x <= 10)
 
-# 논리 연산자 
+# 논리 연산
 x <- c(TRUE, TRUE,  FALSE, FALSE)
 
 y <- c(TRUE, FALSE, TRUE,  FALSE)
@@ -111,7 +111,7 @@ x | y
 
 xor(x, y)
 
-# 결측지와 벌
+# 결측지
 x <- NULL
 is.null(x)
 
@@ -124,16 +124,12 @@ z
 u <- 0/0
 u
 
-# 3.3 요인 
-
 # 요인 만들기 
 gender <- c("남", "여", "남")
 gender
 
 gender.factor <- factor(gender)
 gender.factor
-
-# 3.4 배열과 행렬
 
 # 배열 만들기
 x <- c(70, 80, 95)
@@ -178,9 +174,7 @@ z <- c(10, 11, 12)
 mtx <- cbind(mtx, z)
 mtx
 colnames(mtx)[4] <- "4열"
-mtx
-
-# 3.5 리스트 반들기 
+mtx 
 
 # 리스트 만들기
 x <- list("알라딘", 20, c(70, 80))
@@ -197,7 +191,7 @@ x[1]    # x["성명"]
 
 x[[1]]  # x[["성명"]], x$성명 
 
-# 3.6 데이터 프레임
+# 데이터 프레임
 
 # 두 고객의 데이터 프레임 만들기
 df <- data.frame(성명=c("알라딘", "자스민"), 
@@ -212,7 +206,7 @@ df
 df <- rbind(df, data.frame(성명="지니", 나이=30, 국어=90, 음악=75))
 df
 
-# 요소 값 보기
+# 요소 값 확인
 df[3, 2]
 
 df[3,]
@@ -221,7 +215,7 @@ df[, 2]
 
 df[-2,]
 
-# 요소 값 수정
+# 요소 값 정정
 df <- data.frame(성명=c("알라딘", "자스민"), 나이=c(20, 19), 국어=c(70, 85))
 str(df)
 df
@@ -239,12 +233,12 @@ str(df)
 df[1,1] <- "Aladin"
 df
 
-# 3.7 데이터 파일 읽기
+# 데이터 파일 읽기
 
-# 데이터 세트 목록
+# 데이터 세트 목록 확인
 data(package = "datasets")
 
-# 데이터 세트 보기
+# 데이터 세트 확인
 quakes
 
 head(quakes)
@@ -262,14 +256,14 @@ str(quakes)
 
 summary(quakes)
 
-# 데이터 세트 저장하고 읽기
+# 데이터 세트 저장/읽기
 write.table(quakes, "c:/Temp/quakes.csv", sep=",")
 df <- read.csv("c:/Temp/quakes.csv", header=T)
 df
 
 # 3.8 함수
 
-# 함수 정의와 호출
+# 함수 정의와 호출 (예제 : 삼각형 넓이 공식)
 getTriangleArea <- function(w, h) {
   area <- w * h / 2
   return(area)
